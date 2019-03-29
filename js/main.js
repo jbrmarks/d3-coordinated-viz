b6f4900d2bd04988082560faec848313a003ba20
jbrmarks <jbrmarks@gmail.com>
2019-03-29 16:08:58 -0500

    Module 8 assignment completed

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ function setMap() {
     
     // Create Albers equal area conic projection centered on Chicago, IL
     var projection = d3.geoAlbers()
-        .center([0, 41.8781])
+        .center([0, 41.8419])
         .rotate([87.6298, 0])
         .parallels([41.583333, 42.133333])
         .scale(100000)
@@ -34,7 +34,7 @@ function setMap() {
     // Use Promise.all to parallelize asynchronous data loading
     var promises = [];
     promises.push(d3.csv("data/Chicago_500_cities_data.csv")); // Load attributes from csv
-    //promises.push(d3.json("data/ne_10m_admin_1_states_provin_WGS84.topojson")); // Load background spatial data
+    //promises.push(d3.json("data/ne_10m_admin_1_states_provin_WGS84.topojson")); // Load background spatial data (A different possible basemap)
     promises.push(d3.json("data/Background_Tracts_WGS84.topojson")); // Load background spatial data (tract level)
     promises.push(d3.json("data/Chicago_Census_Tracts_WGS84.topojson")); // Load census tract data
     
@@ -44,24 +44,29 @@ function setMap() {
         csvData = data[0];
         baseMap = data[1]; 
         chicagoTracts = data[2]; 
-
-        console.log("csvData: ");
-        console.log(csvData);
-        console.log("baseMap: ");
-        console.log(baseMap);
-        console.log("chicagoTracts: ");
-        console.log(chicagoTracts);
         
+        // Create graticule generator
+        var graticule = d3.geoGraticule()
+            .step([.15, .15]); // Place graticule lines every .15 degree of longitude and latitude
+        
+        // Create graticule background
+        var gratBackground = map.append("path")
+            .datum(graticule.outline()) // Bind graticule background
+            .attr("class", "gratBackground") // Assign class for styling
+            .attr("d", path); // Project graticule
+        
+        // Create graticule lines
+        var gratLines = map.selectAll(".gratLines") // Select graticule elements that will be created
+            .data(graticule.lines()) // Bind graticule lines to each element to be created
+            .enter() // Create an element for each datum
+            .append("path") // Append each element to the svg as a path element
+            .attr("class", "gratLines") // Assign class for styling
+            .attr("d", path); // Project graticule lines
         
         // Translate census tracts TopoJSON
-        //var baseMapGeoJson = topojson.feature(baseMap, baseMap.objects.ne_10m_admin_1_states_provin_WGS84);
+        //var baseMapGeoJson = topojson.feature(baseMap, baseMap.objects.ne_10m_admin_1_states_provin_WGS84); // (A different possible basemap)
         var baseMapGeoJson = topojson.feature(baseMap, baseMap.objects.Background_Tracts_WGS84);
         var chicagoTractsGeoJsonFeatures = topojson.feature(chicagoTracts, chicagoTracts.objects.Chicago_Census_Tracts_WGS84).features;
-        
-        console.log("baseMapGeoJson: ");
-        console.log(baseMapGeoJson);
-        console.log("chicagoTractsGeoJsonFeatures: ");
-        console.log(chicagoTractsGeoJsonFeatures);
         
         // Add countries to the map
         var baseMapPath = map.append("path")
@@ -80,9 +85,9 @@ function setMap() {
             .attr("d", path);
         
         
-        console.log("baseMapPath: ");
-        console.log(baseMapPath);
-        console.log("chicagoTractsPath: ");
-        console.log(chicagoTractsPath);
+        
+        console.log("gratLines: ");
+        console.log(gratLines);
+        
     }
 }
