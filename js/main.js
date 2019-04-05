7d820d55f5facb168e9eae72e331a03aaacf675f
jbrmarks <jbrmarks@gmail.com>
2019-04-05 17:05:21 -0500

    Module 9 assignment completed

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@
 (function(){
 
     //pseudo-global variables
-    var attrArray = ["Population2010","ACCESS2_CrudePrev","ARTHRITIS_CrudePrev","BINGE_CrudePrev","BPHIGH_CrudePrev","BPMED_CrudePrev","CANCER_CrudePrev","CASTHMA_CrudePrev","CHD_CrudePrev","CHECKUP_CrudePrev","CHOLSCREEN_CrudePrev","COLON_SCREEN_CrudePrev","COPD_CrudePrev","COREM_CrudePrev","COREW_CrudePrev","CSMOKING_CrudePrev","DENTAL_CrudePrev","DIABETES_CrudePrev","HIGHCHOL_CrudePrev","KIDNEY_CrudePrev","LPA_CrudePrev","MAMMOUSE_CrudePrev","MHLTH_CrudePrev","OBESITY_CrudePrev","PAPTEST_CrudePrev","PHLTH_CrudePrev","SLEEP_CrudePrev","STROKE_CrudePrev","TEETHLOST_CrudePrev"];
+    var attrArray = ["Population2010","Lack of Health Insurance","Arthritis,Binge Drinking","High Blood Pressure","People Taking Medicine for High Blood Pressure","Cancer (Excluding Skin Cancer)","Asthma,Coronary Heart Disease","Visits to Doctor for Routine Checkup within the Past Year","Cholesterol Screening","Colon Screening","Chronic Obstructive Pulmonary Disease","Men >= 65 Years up to Date on Core Set of Clinical Preventive Services","Women >= 65 Years up to Date on Core Set of Clinical Preventive Services","Smoking","Visits to Dentist","Diabetes","High Cholesterol","Chronic Kidney Disease","No Leisure-Time Physical Activity","Mammography Use among Women aged 50-74 years","Mental Health Not Good","Obesity","Papanicolaou Smear Use Among Adult Women Aged 21-65 Years","Physical Health Not Good","Sleeping Less Than 7 Hours","Stroke","Teeth Lost Among Adults Aged >= 65 Years"];
     //list of attributes
-    var expressed = attrArray[1]; //initial attribute; 29 attributes
+    var expressed = attrArray[3]; //initial attribute; 29 attributes
 
 
     //begin script when window loads
@@ -28,12 +28,12 @@
             .attr("width", width)
             .attr("height", height);
 
-        // Create Albers equal area conic projection centered on Chicago, IL
+        // Create Albers equal area conic projection centered on Green Bay, WI
         var projection = d3.geoAlbers()
-            .center([0, 41.8419])
-            .rotate([87.7298, 0])
-            .parallels([41.583333, 42.133333])
-            .scale(100000)
+            .center([0, 44.5133])
+            .rotate([88.0000, 0])
+            .parallels([44.30, 44.31])
+            .scale(220000)
             .translate([width / 2, height / 2]);
 
         var path = d3.geoPath()
@@ -43,17 +43,23 @@
 
         // Use Promise.all to parallelize asynchronous data loading
         var promises = [];
+        
+        /* Leftover from when I attempted to do Chicago's census tracts (too many tracts made it difficult to visualize)
         promises.push(d3.csv("data/Chicago_500_cities_data.csv")); // Load attributes from csv
         //promises.push(d3.json("data/ne_10m_admin_1_states_provin_WGS84.topojson")); // Load background spatial data (A different possible basemap)
         promises.push(d3.json("data/Background_Tracts_WGS84.topojson")); // Load background spatial data (tract level)
-        promises.push(d3.json("data/Chicago_Census_Tracts_WGS84.topojson")); // Load census tract data
+        promises.push(d3.json("data/Chicago_Census_Tracts_WGS84.topojson")); // Load census tract data*/
+        
+        promises.push(d3.csv("data/Green Bay 500 Cities Data.csv")); // Load attributes from csv
+        promises.push(d3.json("data/Brown_County_Census_Tracts_WGS84.topojson")); // Load background spatial data (tract level)
+        promises.push(d3.json("data/Green_Bay_Census_Tracts_WGS84.topojson")); // Load census tract data
 
         Promise.all(promises).then(callback);
 
         function callback(data){
             csvData = data[0];
             baseMap = data[1]; 
-            chicagoTracts = data[2]; 
+            greenBayTracts = data[2]; 
 
             // Place graticule on the map
             setGraticule(map, path);
@@ -61,11 +67,11 @@
 
             // Translate census tracts TopoJSONs
             //var baseMapGeoJson = topojson.feature(baseMap, baseMap.objects.ne_10m_admin_1_states_provin_WGS84); // (A different possible basemap)
-            var baseMapGeoJson = topojson.feature(baseMap, baseMap.objects.Background_Tracts_WGS84);
-            var chicagoTractsGeoJsonFeatures = topojson.feature(chicagoTracts, chicagoTracts.objects.Chicago_Census_Tracts_WGS84).features;
+            var baseMapGeoJson = topojson.feature(baseMap, baseMap.objects.Brown_County_Census_Tracts_WGS84);
+            var greenBayTractsGeoJsonFeatures = topojson.feature(greenBayTracts, greenBayTracts.objects.Green_Bay_Census_Tracts_WGS84).features;
 
 
-            chicagoTractsGeoJsonFeatures = joinData(chicagoTractsGeoJsonFeatures, csvData);
+            greenBayTractsGeoJsonFeatures = joinData(greenBayTractsGeoJsonFeatures, csvData);
             
             
             // Add countries to the map
@@ -77,8 +83,8 @@
             // Create the color scale
             var colorScale = createColorScale(csvData);
             
-            // Add Chicago census tracts to map
-            setEnumerationUnits(chicagoTractsGeoJsonFeatures, map, path, colorScale);
+            // Add Green Bay census tracts to map
+            setEnumerationUnits(greenBayTractsGeoJsonFeatures, map, path, colorScale);
 
             // Add coordinated visualization to the map
             setChart(csvData, colorScale);
@@ -106,17 +112,17 @@
             .attr("d", path); // Project graticule lines
     }
     
-    function joinData(chicagoTractsGeoJsonFeatures, csvData){
+    function joinData(greenBayTractsGeoJsonFeatures, csvData){
         // Loop through csv to assign each set of scv attribute values to geojson region
         for (var i = 0; i < csvData.length; i++){
             var csvRegion = csvData[i]; // The current region
             var csvKey = csvRegion.Place_TractID // The CSV primary key
 
             // Loop through geojson regions to find correct region
-            for(var j = 0; j < chicagoTractsGeoJsonFeatures.length; j++){
+            for(var j = 0; j < greenBayTractsGeoJsonFeatures.length; j++){
 
                 // Current region geojson properties
-                var geojsonProps = chicagoTractsGeoJsonFeatures[j].properties;
+                var geojsonProps = greenBayTractsGeoJsonFeatures[j].properties;
                 // Geojson primary key
                 var geojsonKey = geojsonProps.plctract10;
 
@@ -130,7 +136,7 @@
                 }
             }
         }
-        return chicagoTractsGeoJsonFeatures;
+        return greenBayTractsGeoJsonFeatures;
     }
     
     function createColorScale(csvData){
@@ -159,14 +165,14 @@
         return colorScale;
     };
     
-    function setEnumerationUnits(chicagoTractsGeoJsonFeatures, map, path, colorScale){
-        // Add Chicago census tracts to map
-        var chicagoTractsPath = map.selectAll(".chicagoTractsPath")
-            .data(chicagoTractsGeoJsonFeatures)
+    function setEnumerationUnits(greenBayTractsGeoJsonFeatures, map, path, colorScale){
+        // Add Green Bay census tracts to map
+        var greenBayTractsPath = map.selectAll(".greenBayTractsPath")
+            .data(greenBayTractsGeoJsonFeatures)
             .enter()
             .append("path")
             .attr("class", function(d){
-                return "chicagoTractsPath " + d.properties.plctract10
+                return "greenBayTractsPath " + d.properties.plctract10
             })
             .attr("d", path)
             .style("fill", function(d){
@@ -182,7 +188,7 @@
         if (typeof val ==  'number' && !isNaN(val)){
             return colorScale(val);
         }else{
-            return '#CCC';
+            return '#BBB';
         }
     };
     
@@ -191,7 +197,13 @@
     function setChart(csvData, colorScale){
         // chart frame dimensions
         var chartWidth = window.innerWidth * 0.425,
-            chartHeight = 860;
+            chartHeight = 860,
+            leftPadding = 25,
+            rightPadding = 2,
+            topBottomPadding = 5,
+            chartInnerWidth = chartWidth - leftPadding - rightPadding,
+            chartInnerHeight = chartHeight - topBottomPadding * 2,
+            translate = "translate(" + leftPadding + ", " + topBottomPadding + ")";
         
         // Create a second svg element to hold the var chart
         var chart = d3.select("body")
@@ -200,9 +212,16 @@
             .attr("height", chartHeight)
             .attr("class", "chart");
         
+        // Create a rectangle for chart background fill
+        var chartBackground = chart.append("rect")
+            .attr("class", "chartBackground")
+            .attr("width", chartInnerWidth)
+            .attr("height", chartInnerHeight)
+            .attr("transform", translate);
+        
         // Create a scale to size bars proportionally to frame
         var yScale = d3.scaleLinear()
-            .range([0, chartHeight])
+            .range([850, 0])
             .domain([0, 50]);
         
         // Set bars for each tract
@@ -216,18 +235,67 @@
             .attr("class", function(d){
                 return "bars " + d.Place_TractID
             })
-            .attr("width", chartWidth/csvData.length - .75)
+            .attr("width", chartInnerWidth/csvData.length - 1)
             .attr("x", function(d, i){
-                return i * (chartWidth/csvData.length);
+                return i * (chartInnerWidth/csvData.length) + leftPadding;
             })
             .attr("height", function(d){
-                return yScale(parseFloat(d[expressed]));
+                return 850 - yScale(parseFloat(d[expressed]));
             })
             .attr("y", function(d){
-                return chartHeight - yScale(parseFloat(d[expressed]));
+                return yScale(parseFloat(d[expressed])) + topBottomPadding;
             })
             .style("fill", function(d){
                 return choropleth(d, colorScale);
             });
+        
+        /*
+        // Annotate bars with attribute value text
+        var numbers = chart.selectAll(".numbers")
+            .data(csvData)
+            .enter()
+            .append("text")
+            .sort(function(a, b){
+                return a[expressed]-b[expressed];
+            })
+            .attr("class", function (d){
+                return "numbers " + d.Place_TractID
+            })
+            .attr("text-anchor", "middle")
+            .attr("x", function(d, i){
+                var fraction = chartWidth / csvData.length;
+                return i * fraction + (fraction-1)/2;
+            })
+            .attr("y", function(d){
+                return chartHeight - yScale(parseFloat(d[expressed])) + 15;
+            })
+            .text(function(d){
+                return d[expressed];
+            });
+        */
+        
+        // Create a text element for the chart title
+        var chartTitle = chart.append("text")
+            .attr("x", 40)
+            .attr("y", 40)
+            .attr("class", "chartTitle")
+            .text("Prevalence of " + expressed + " in Each Census Tract");
+        
+        // Create vertical axis generator
+        var yAxis = d3.axisLeft()
+            .scale(yScale);
+        
+        // Place axis
+        var axis = chart.append("g")
+            .attr("class", "axis")
+            .attr("transform", translate)
+            .call(yAxis);
+        
+        // Create frame for chart border
+        var chartFrame = chart.append("rect")
+            .attr("class", "chartFrame")
+            .attr("width", chartInnerWidth)
+            .attr("height", chartInnerHeight)
+            .attr("transform", translate);
     };
 })() // Last line of main.js
